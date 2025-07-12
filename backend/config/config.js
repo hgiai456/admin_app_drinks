3e89c45b09099ea9dd32aa461ed115342d323ec3
23thyu <hieungao2004@gmail.com>
2025-07-12 14:18:57 +0700

    done

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
-export const development = {
+const development = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD || null,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
   dialect: process.env.DB_DIALECT || "mysql",
+};
+
+module.exports = {
+  development,
 };
