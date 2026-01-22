5ff1a60f6bea64af677f4eeb1101b569eaa8f06f
Hoa Giai <damhoagiai456@gmail.com>
2026-01-22 14:28:14 +0700

    fix config

--- backend/dtos/requests/cart/InsertCartRequest.js
+++ backend/dtos/requests/cart/InsertCartRequest.js
@@ -1,19 +1,19 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 class InsertCartRequest {
-    constructor(data) {
-        this.user_id = data.user_id; // Image is optional
-        this.session_id = data.session_id;
-    }
+  constructor(data) {
+    this.user_id = data.user_id; // Image is optional
+    this.session_id = data.session_id;
+  }
 
-    static validate(data) {
-        const schema = Joi.object({
-            user_id: Joi.number().integer().optional(),
-            session_id: Joi.string().optional().allow('')
-        });
+  static validate(data) {
+    const schema = Joi.object({
+      user_id: Joi.number().integer().optional(),
+      session_id: Joi.string().optional().allow(""),
+    });
 
-        return schema.validate(data); // Returns { error, value }
-    }
+    return schema.validate(data); // Returns { error, value }
+  }
 }
 
 export default InsertCartRequest;
--- backend/dtos/requests/user/LoginUserRequest.js
+++ backend/dtos/requests/user/LoginUserRequest.js
@@ -1,25 +1,24 @@
-import Joi from 'joi';
-import { UserRole } from '../../../constants';
+import Joi from "joi";
 
 class LoginUserRequest {
-    constructor(data) {
-        this.email = data.email;
-        this.password = data.password;
-        this.phone = data.phone || null;
-    }
+  constructor(data) {
+    this.email = data.email;
+    this.password = data.password;
+    this.phone = data.phone || null;
+  }
 
-    static validate(data) {
-        const schema = Joi.object({
-            email: Joi.string().email().optional(),
-            password: Joi.string().min(6).required(), // Đảm bảo phải gọi mã hoá trước khi lưu!
-            phone: Joi.string()
-                .pattern(/^(0|\+84)[0-9]{9,10}$/)
-                .optional()
-                .allow(null, '')
-        });
+  static validate(data) {
+    const schema = Joi.object({
+      email: Joi.string().email().optional(),
+      password: Joi.string().min(6).required(), // Đảm bảo phải gọi mã hoá trước khi lưu!
+      phone: Joi.string()
+        .pattern(/^(0|\+84)[0-9]{9,10}$/)
+        .optional()
+        .allow(null, ""),
+    });
 
-        return schema.validate(data);
-    }
+    return schema.validate(data);
+  }
 }
 
 export default LoginUserRequest;
