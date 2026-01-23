c138f467302ffa7db3cfbb45f6a3a9be19e040ad
Hoa Giai <damhoagiai456@gmail.com>
2026-01-23 19:12:17 +0700

    next commit

--- backend/migrations/20250605121008-rename-productid-column-prodetails.cjs
+++ backend/migrations/20250605121008-rename-productid-column-prodetails.cjs
@@ -1,17 +1,17 @@
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.renameColumn(
-            'prodetails',
-            'prouduct_id',
-            'product_id'
-        );
-    },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.renameColumn(
+      "prodetails",
+      "prouduct_id",
+      "product_id",
+    );
+  },
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.renameColumn(
-            'prodetails',
-            'prouduct_id',
-            'product_id'
-        );
-    }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.renameColumn(
+      "prodetails",
+      "prouduct_id",
+      "product_id",
+    );
+  },
 };
