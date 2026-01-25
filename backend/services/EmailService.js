574af810c06dd2848c8948cda3c90b7810169463
Hoa Giai <damhoagiai456@gmail.com>
2026-01-25 17:04:38 +0700

    Next commit

--- backend/services/EmailService.js
+++ backend/services/EmailService.js
@@ -5,16 +5,13 @@ dotenv.config();
 class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.BREVO_SMTP_USER,
+        pass: process.env.BREVO_SMTP_KEY,
       },
-
-      logger: true,
-      debug: true,
     });
   }
 
