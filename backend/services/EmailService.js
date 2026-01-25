0b96235b35e1b25d9e103e01555be1a8eb24c191
Hoa Giai <damhoagiai456@gmail.com>
2026-01-25 16:46:06 +0700

    fix: conflict sendEmailConfirmation

--- backend/services/EmailService.js
+++ backend/services/EmailService.js
@@ -5,13 +5,16 @@ dotenv.config();
 class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: "smtp.gmail.com",
+      host: "smtp.gmail.com",
       port: 465,
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      logger: true,
+      debug: true,
     });
   }
 
