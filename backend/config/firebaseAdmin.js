2fdd795f70dd16e6a7c2cd1d56ba242c318f8f5c
Hoa Giai <damhoagiai456@gmail.com>
2026-01-28 13:45:13 +0700

    Fix: Firebase Storage Conflicts

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,11 +1,26 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-service-account.json";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import dotenv from "dotenv";
 
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const serviceAccount = JSON.parse(
+  readFileSync(join(__dirname, "./firebase-service-account.json"), "utf8"),
+);
+
+const storageBucket =
+  process.env.FIREBASE_STORAGE_BUCKET || "hg-store-a11c5.appspot.com";
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    databaseURL:
+      "https://hg-store-a11c5-default-rtdb.asia-southeast1.firebasedatabase.app",
     storageBucket: storageBucket,
   });
 }
